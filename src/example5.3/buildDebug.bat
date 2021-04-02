18b0ff06115871328b8c262e1349a65b6effbce7
William Emerison Six <billsix@gmail.com>
2021-04-02 06:57:34 -0400

    Added more from the book.

---
+++ src/example5.3/buildDebug.bat
@@ -0,0 +1,9 @@
+mkdir build
+mkdir buildInstall
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=..\buildInstall -DCMAKE_BUILD_TYPE=Debug ..\
+cmake --build  . --target all
+cmake --build  . --target install
+
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" example5.3.sln
