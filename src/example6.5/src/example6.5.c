1b0a6b7d613f792a33e2279da4b6c023d9bf5248
William Emerison Six <billsix@gmail.com>
2021-04-09 18:01:58 -0400

    Added build scripts and ran clang-format

--- src/example6.5/src/example6.5.c
+++ src/example6.5/src/example6.5.c
@@ -1,16 +1,16 @@
 
-struct list_ele{
-      int data;
-      struct list_ele *pointer;
-}ar[3];
+struct list_ele {
+  int data;
+  struct list_ele *pointer;
+} ar[3];
 
-main(){
+int main() {
 
-      ar[0].data = 5;
-      ar[0].pointer = &ar[1];
-      ar[1].data = 99;
-      ar[1].pointer = &ar[2];
-      ar[2].data = -7;
-      ar[2].pointer = 0;      /* mark end of list */
-      return(0);
+  ar[0].data = 5;
+  ar[0].pointer = &ar[1];
+  ar[1].data = 99;
+  ar[1].pointer = &ar[2];
+  ar[2].data = -7;
+  ar[2].pointer = 0; /* mark end of list */
+  return 0;
 }
