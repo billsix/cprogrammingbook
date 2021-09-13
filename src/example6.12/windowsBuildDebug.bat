323510e830100d44892661f7aa6b35ec1f8bdf3c
William Emerison Six <billsix@gmail.com>
2021-09-12 20:31:14 -0500

    removed old build files

--- src/example6.12/windowsBuildDebug.bat
+++
@@ -1,9 +0,0 @@
-mkdir build
-mkdir buildInstall
-cd build
-
-cmake -DCMAKE_INSTALL_PREFIX=..\buildInstall -DCMAKE_BUILD_TYPE=Debug ..\
-cmake --build  . --target all
-cmake --build  . --target install
-
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" example6.12.sln
